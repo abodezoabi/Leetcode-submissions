02259a05098343f256ef2ba3c9381856c5bea8e5
Zoabi Abedallah <122079462+abodezoabi@users.noreply.github.com>
2026-04-20 23:15:12 +0300

    Time: 224 ms (97.13%), Space: 21.7 MB (51.49%) - LeetHub

---
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -0,0 +1,26 @@
+class Solution:
+    def numIslands(self, grid: List[List[str]]) -> int:
+        rows, cols = len(grid), len(grid[0])
+        count = 0
+
+        def dfs(r, c):
+            # out of bounds OR water
+            if r < 0 or c < 0 or r >= rows or c >= cols or grid[r][c] == "0":
+                return
+
+            # mark as visited
+            grid[r][c] = "0"
+
+            # explore 4 directions
+            dfs(r+1, c)  # down
+            dfs(r-1, c)  # up
+            dfs(r, c+1)  # right
+            dfs(r, c-1)  # left
+
+        for r in range(rows):
+            for c in range(cols):
+                if grid[r][c] == "1":
+                    dfs(r, c)
+                    count += 1
+
+        return count
