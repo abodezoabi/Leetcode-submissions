5e6de6991a09f2911376d1fc216f4e3a9710634c
Zoabi Abedallah <122079462+abodezoabi@users.noreply.github.com>
2026-08-01 16:28:24 +0300

    Time: 23 ms (5.54%), Space: 19.6 MB (76.37%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -4,10 +4,10 @@ class Solution:
         l,r= 0, len(s)-1
         
         while l < r:
-            while l < r and  not self.isAlpha(s[l]):
+            while l < r and  not self.isAlphaNumeric(s[l]):
                     l+=1
 
-            while r > l and  not self.isAlpha(s[r]):
+            while r > l and  not self.isAlphaNumeric(s[r]):
                     r-=1
 
             if s[l].lower() != s[r].lower():
@@ -19,7 +19,7 @@ class Solution:
         return True
 
 
-    def isAlpha(self, c):
+    def isAlphaNumeric(self, c):
          return ( (ord('A') <= ord(c) <= ord('Z')) or
                   (ord('a') <= ord(c) <= ord('z')) or
                   (ord('0') <= ord(c) <= ord('9')) )
